1d5167c7cd0965a0319da4d479efa1cbc6a5559f
Michael Eickenberg <michael.eickenberg@gmail.com>
2014-02-19 00:22:05 +0100

    morlet filter

---
+++ filters/morlet_2d_noDC.py
@@ -0,0 +1,30 @@
+"""Makes a Morlet Wavelet by removing the DC offset from Gabor wavelet"""
+from gabor_2d import gabor_2d
+import numpy as np
+
+
+def morlet_2d_noDC(shape, sigma, xi, theta, slant=None, offset=None):
+
+    gabor = gabor_2d(shape, sigma, xi, theta, slant, offset)
+    envelope = np.abs(gabor)
+    K = gabor.sum() / envelope.sum()
+
+    centered = gabor - K * envelope
+
+    return centered
+
+
+if __name__ == "__main__":
+    
+    mor = morlet_2d_noDC([512, 512], 12, np.pi / 16, np.pi / 6, 1. / 4.)
+
+    import pylab as pl
+    pl.figure()
+    pl.subplot(1, 2, 1)
+    pl.imshow(np.fft.fftshift(np.real(mor)))
+    pl.axis('off')
+    pl.gray()
+    pl.subplot(1, 2, 2)
+    pl.imshow(np.fft.fftshift(np.imag(mor)))
+    pl.axis('off')
+
